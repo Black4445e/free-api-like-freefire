b3ce388879584f31a35933edebd80458f24f4f84
Black4445e <brunomiguelsilvagomes19@gmail.com>
2025-07-20 07:02:11 +0100

    Update like_routes.py

--- app/like_routes.py
+++ app/like_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, send_from_directory
 import asyncio
 from datetime import datetime, timezone
 import logging
